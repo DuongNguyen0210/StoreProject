d676f439c4b626d992cae7fc341938e87b74cd3f
Duong Nguyen <zkoclompe02@gmail.com>
2025-11-18 10:57:30 +0700

    một vài thứ lặt vặt

--- logindialog.cpp
+++ logindialog.cpp
@@ -7,15 +7,20 @@ LoginDialog::LoginDialog(Store* store, QWidget *parent)
 {
     ui->setupUi(this);
 
-    // Ẩn label lỗi ban đầu
     ui->lblError->setVisible(false);
 
-    // Kết nối nút đăng nhập
     connect(ui->btnLogin, &QPushButton::clicked, this, &LoginDialog::onLoginClicked);
 
-    // Cho phép nhấn Enter để đăng nhập
     connect(ui->txtUsername, &QLineEdit::returnPressed, this, &LoginDialog::onLoginClicked);
     connect(ui->txtPassword, &QLineEdit::returnPressed, this, &LoginDialog::onLoginClicked);
+
+    connect(ui->chkShowPassword, &QCheckBox::toggled, this, [this](bool checked)
+    {
+        if (checked)
+            ui->txtPassword->setEchoMode(QLineEdit::Normal);
+        else
+            ui->txtPassword->setEchoMode(QLineEdit::Password);
+    });
 }
 
 LoginDialog::~LoginDialog()
@@ -33,10 +38,10 @@ void LoginDialog::onLoginClicked()
     QString username = ui->txtUsername->text().trimmed();
     QString password = ui->txtPassword->text().trimmed();
 
-    // Ẩn thông báo lỗi cũ
     ui->lblError->setVisible(false);
 
-    // Kiểm tra trống
+    qDebug() << username << " " << password << '\n';
+
     if (username.isEmpty() || password.isEmpty())
     {
         ui->lblError->setText("Vui lòng nhập đầy đủ tên đăng nhập và mật khẩu!");
@@ -44,27 +49,15 @@ void LoginDialog::onLoginClicked()
         return;
     }
 
-    // Tìm user theo tên
     User* user = m_store->findUserByName(username);
 
-    if (user == nullptr)
+    if (user == nullptr || user->getPassword() != password)
     {
-        ui->lblError->setText("Tên đăng nhập không tồn tại!");
+        ui->lblError->setText("Tên đăng nhập hoặc mật khẩu không hợp lệ!");
         ui->lblError->setVisible(true);
-        ui->txtPassword->clear();
         return;
     }
 
-    // Kiểm tra mật khẩu
-    if (user->getPassword() != password)
-    {
-        ui->lblError->setText("Mật khẩu không đúng!");
-        ui->lblError->setVisible(true);
-        ui->txtPassword->clear();
-        return;
-    }
-
-    // Đăng nhập thành công
     m_loggedInUser = user;
     accept();
 }
